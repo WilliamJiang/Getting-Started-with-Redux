6b7acc9737da7fda796df0353c87c526564ac446
William.Jiang <williamjxj@hotmail.com>
2017-04-23 20:57:01 -0400

    loginForm works, directly from redux-form.

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -6,52 +6,43 @@ import 'react-widgets/dist/css/react-widgets.css'
 
 const validateUsers = ['william', 'bill', 'admin', 'root'];
 
-const sleep = ms => {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const submit = values => {
+function submit(values) {
   return sleep(1000) // simulate server latency
-    .then(() => {
-      if (!validateUsers.includes(values.username)) {
-        throw new SubmissionError({username: 'User does not exist', _error: 'Login failed!'})
-      } else if (['redux-form', ''].indexOf(values.password) === -1) {
-        throw new SubmissionError({password: 'Wrong password', _error: 'Login failed!'})
-      } else {
-        window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
-      }
-    })
+      .then(() => {
+        if (![ 'john', 'paul', 'george', 'ringo' ].includes(values.username)) {
+          throw new SubmissionError({ username: 'User does not exist', _error: 'Login failed!' })
+        } else if (values.password !== 'redux-form') {
+          throw new SubmissionError({ password: 'Wrong password', _error: 'Login failed!' })
+        } else {
+          window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+        }
+      })
 }
 
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+    <div>
+      <label>{label}</label>
+      <div>
+        <input {...input} placeholder={label} type={type}/>
+        {touched && error && <span>{error}</span>}
+      </div>
+    </div>
+)
 
 let SubmitValidationForm = (props) => {
   const { error, handleSubmit, pristine, reset, submitting } = props
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username</label>
-        <Field name="username" component={username =>
-          <div>
-            <input type="text" {...username} placeholder="Username"/>
-            {username.touched && username.error && <span>{username.error}</span>}
-          </div>
-        }/>
-      </div>
-      <div>
-        <label>Password</label>
-        <Field name="password" component={password =>
-          <div>
-            <input type="password" {...password} placeholder="Password"/>
-            {password.touched && password.error && <span>{password.error}</span>}
-          </div>
-        }/>
-      </div>
-      {error && <strong>{error}</strong>}
-      <div>
-        <button type="submit" disabled={submitting}>Log In</button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
-      </div>
-    </form>
+      <form onSubmit={handleSubmit(submit)}>
+        <Field name="username" type="text" component={renderField} label="Username"/>
+        <Field name="password" type="password" component={renderField} label="Password"/>
+        {error && <strong>{error}</strong>}
+        <div>
+          <button type="submit" disabled={submitting}>Log In</button>
+          <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+        </div>
+      </form>
   )
 }
 
